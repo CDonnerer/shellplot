1c9582044f9a4cabee129545d7075b59f85b8cac
Christian Donnerer <christian.donnerer@gmail.com>
2020-12-05 21:39:32 +0000

    cleaning up test for tolerance round

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,8 +3,14 @@ import pytest
 from shellplot.utils import tolerance_round
 
 
-def test_round_sensibly():
-    assert tolerance_round(0.399997) == 0.4
-    assert tolerance_round(102.1) == 102
-    assert tolerance_round(1.499997) == 1.5
-    assert tolerance_round(503.4) == 503
+@pytest.mark.parametrize(
+    "number,expected",
+    [
+        (0.399997, 0.4),
+        (102.1, 102),
+        (1.499997, 1.5),
+        (503.4, 503),
+    ]
+)
+def test_tolerance_round(number, expected):
+    assert tolerance_round(number) == expected
