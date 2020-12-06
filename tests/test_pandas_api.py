4b56037187b39a18f2c1bcf8c4efca8e3f9d0374
Christian Donnerer <christian.donnerer@gmail.com>
2020-12-06 16:14:28 +0000

    updated pandas api testing

--- tests/test_pandas_api.py
+++ tests/test_pandas_api.py
@@ -4,12 +4,16 @@ import numpy as np
 import pandas as pd
 
 
-def test_set_shellplot_backend():
+def set_shellplot_plotting_backend():
     pd.set_option("plotting.backend", "shellplot")
+
+
+def test_set_shellplot_backend():
+    set_shellplot_plotting_backend()
 
 
 def test_plot():
-    pd.set_option("plotting.backend", "shellplot")
+    set_shellplot_plotting_backend()
     x = np.arange(-3, 3, 0.1)
     df = pd.DataFrame({"x": x, "y": np.cos(x) ** 2 + x / 5})
     df.plot(x="x", y="y")
