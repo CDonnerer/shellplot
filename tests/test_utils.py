b95bf75fd5848179db0cbd7e4c56169d736d7398
Christian Donnerer <christian.donnerer@gmail.com>
2020-12-12 14:41:31 +0000

    adding manifest for including dataset

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,9 @@
 import pytest
 
-from shellplot.utils import tolerance_round
+import numpy as np
+import pandas as pd
+
+from shellplot.utils import load_dataset, remove_any_nan, tolerance_round
 
 
 @pytest.mark.parametrize(
@@ -18,3 +21,27 @@ def test_tolerance_round(number, expected):
     rounded, _ = tolerance_round(number, tol=1e-1)
 
     assert rounded == expected
+
+
+@pytest.mark.parametrize(
+    "x, y, expected_x, expected_y",
+    [
+        (np.array([0, np.nan]), np.array([1, 2]), np.array([0]), np.array([1])),
+        (np.array([0, 1]), np.array([np.nan, 2]), np.array([1]), np.array([2])),
+        (np.array([0, np.nan]), np.array([1, np.nan]), np.array([0]), np.array([1])),
+    ],
+)
+def test_remove_any_nan(x, y, expected_x, expected_y):
+    no_nan_x, no_nan_y = remove_any_nan(x, y)
+
+    np.testing.assert_equal(no_nan_x, expected_x)
+    np.testing.assert_equal(no_nan_y, expected_y)
+
+
+@pytest.mark.parametrize(
+    "name",
+    [("penguins")],
+)
+def test_load_dataset(name):
+    df = load_dataset(name)
+    assert isinstance(df, pd.DataFrame)
