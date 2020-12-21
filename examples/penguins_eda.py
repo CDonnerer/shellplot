b1066fe8ac5b5fb45628e48f9378799afcb78f8e
Christian Donnerer <christian.donnerer@gmail.com>
2020-12-21 18:07:03 +0000

    updated docs with new axis

--- examples/penguins_eda.py
+++ examples/penguins_eda.py
@@ -12,12 +12,9 @@ df["species"].value_counts().plot.barh()
 
 df[["species", "island"]].value_counts().plot.barh()
 
-
 df["body_mass_g"].hist(bins=10)
 
-
 plt.plot(df["bill_length_mm"], df["flipper_length_mm"], color=df["species"])
-
 
 df.dropna().plot("bill_depth_mm", "body_mass_g", color="species")
 
